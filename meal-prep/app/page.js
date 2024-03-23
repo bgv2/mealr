468b1c84ce8806eef8f15b4ff80e01aba3f6c0cd
aryan1fatemi <sf84561@email.vccs.email.edu>
2024-03-23 17:35:45 -0400

    navbar

--- meal-prep/app/page.js
+++ meal-prep/app/page.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
-
+import Nav from "@/components/Nav";
 export default function Home() {
   return (
     <main className="">
+      <Nav/>
       <h1 className="text-4xl font-extrabold">👋 Welcome to Mealr</h1>
       
     </main>
