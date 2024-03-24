d89d3fd8e78ca6f28f9cfd99310a26b16ca9666d
BGV <26331505+ed789d0@users.noreply.github.com>
2024-03-24 11:42:15 -0400

    added sources and more recipe tweaks

--- meal-prep/app/page.js
+++ meal-prep/app/page.js
@@ -8,6 +8,7 @@ import MealPlanResult from "@/components/MealPlanResult";
 
 export default function Home() {
   const exampleNutrition = <>
+    <h3>Placeholder</h3>
     <table>
       <td style={{ fontWeight: "bold" }}>
         <tr>Calories</tr>
@@ -39,12 +40,13 @@ export default function Home() {
         <h1 className="text-4xl font-extrabold m-5">Your Meal Plan</h1>
         <h2 className="text-2xl font-bold m-5">Sunday</h2>
         <div className="flex space-x-3">
-          <Recipe food="Omelette" description="Eggs, cheese, spinach, ..." nutrition={exampleNutrition} />
+          <Recipe food="Omelette" description="Eggs, cheese, spinach, ..." nutrition={exampleNutrition}
+          url="https://www.allrecipes.com/thmb/fbq_xsA7uXpXZ9CG0ZsT4i70gGc=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/284899spinach-omelette-with-leftover-mashed-potatoesKimKnott4x3-c616844b1ae3408eb3c6f0c0686327de.jpg" />
           <Recipe food="Pizza" description="Cheese, marinara sauce, flour, oil, ..."
             url="https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg"
             nutrition={exampleNutrition}
           />
-          <Recipe food="Pasta" description="Noodles, sauce, cheese, ..." nutrition={exampleNutrition} />
+          <Recipe food="Pasta" description="Noodles, sauce, cheese, ..." nutrition={exampleNutrition} url="https://www.allrecipes.com/thmb/TLZs83FimfwATbwnpBKelVQ-elY=/0x512/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/14385-pasta-salad-DDMFS-4x3-28eb5dbe00624780b36cabfef15ca183.jpg" />
         </div>
         <Footer />
       </div>
