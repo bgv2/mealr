34a04c376d387adccdd843dd95c5729172ca9e6a
aryan1fatemi <sf84561@email.vccs.email.edu>
2024-03-23 22:48:59 -0400

    banner

--- meal-prep/app/page.js
+++ meal-prep/app/page.js
@@ -1,13 +1,11 @@
 import Image from "next/image";
 import Nav from "@/components/Nav";
+import Banner from "@/components/Banner";
 export default function Home() {
   return (
     <>
-    <Nav /><main className="m-5 grid h-screen place-items-left"> <div>
-      <h1 className="text-4xl font-extrabold m-5">👋 Welcome to Mealr</h1>
-      <p className="text-lg m-5 mt-0">Your meal-planning companion.</p>
-      <a className="m-5 mt-0 btn bg-black text-white hover:bg-gray-800 py-2 px-4 rounded transition-colors">Get started 🡢</a>
-      </div></main>
+    <Nav />
+    <Banner/>
     </>
   );
 }
---
+++ meal-prep/components/Banner.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <main className="m-5 grid h-screen place-items-left">
+    <div>
+        <h1 className="text-4xl font-extrabold m-5">👋 Welcome to Mealr</h1>
+        <p className="text-lg m-5 mt-0">Your meal-planning companion.</p>
+        <a className="m-5 mt-0 btn bg-black text-white hover:bg-gray-800 py-2 px-4 rounded transition-colors">Get started 🡢</a>
+        </div>
+    </main>
+  )
+}
+
+export default Banner
