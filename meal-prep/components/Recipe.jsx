81844116a5ee143ec424d9bef433b5be39a83837
aryan1fatemi <sf84561@email.vccs.email.edu>
2024-03-24 01:11:49 -0400

    modals

--- meal-prep/components/Recipe.jsx
+++ meal-prep/components/Recipe.jsx
@@ -1,18 +1,35 @@
+'use client';
 import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { Button, CardActionArea, CardActions } from '@mui/material';
-
+import { Button, CardActionArea, CardActions , Modal, Box} from '@mui/material';
+import { useState } from 'react';
 export default function MultiActionAreaCard(props) {
+    const style = {
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        width: 400,
+        bgcolor: 'background.paper',
+        border: '2px solid #000',
+        boxShadow: 24,
+        p: 4,
+      };
+    const [open, setOpen] = useState(false);
+
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardActionArea>
         <CardMedia
           component="img"
           height="140"
-          image="/assets/logo.png"
+          image={props.url}
           alt="food"
         />
         <CardContent>
@@ -28,6 +45,24 @@ export default function MultiActionAreaCard(props) {
         <Button size="small" color="primary">
           Close
         </Button>
+        <div>
+            <Button onClick={handleOpen}>Info</Button>
+            <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+            >
+            <Box sx={style}>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+                Nutrition Facts
+            </Typography>
+            <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                {props.nutrition}
+            </Typography>
+            </Box>
+        </Modal>
+        </div>
       </CardActions>
     </Card>
   );
