850d91b1a4c1c5712fb201b6874000717563b05b
BGV <26331505+ed789d0@users.noreply.github.com>
2024-03-24 02:18:56 -0400

    Add MealPlanResult component and update footer text

--- meal-prep/app/page.js
+++ meal-prep/app/page.js
@@ -3,6 +3,7 @@ import Nav from "@/components/Nav";
 import Banner from "@/components/Banner";
 import Recipe from "@/components/Recipe";
 import Footer from "@/components/Footer";
+import MealPlanResult from "@/components/MealPlanResult";
 
 
 export default function Home() {
@@ -10,6 +11,7 @@ export default function Home() {
     <main className="bg-gradient-to-b from-slate-100 via-slate-200 to-slate-300">
       <Nav />
       <Banner/>
+      <MealPlanResult/>
       <Recipe food = "Pizza" description="abcd" url="https://www.foodandwine.com/thmb/Wd4lBRZz3X_8qBr69UOu2m7I2iw=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/classic-cheese-pizza-FT-RECIPE0422-31a2c938fc2546c9a07b7011658cfd05.jpg"/>
       <Footer/>
     </main>
