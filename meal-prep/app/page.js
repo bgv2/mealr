981bfffe68a8bdc84f68979aaf8e4793ee35de73
aryan1fatemi <sf84561@email.vccs.email.edu>
2024-03-23 23:26:07 -0400

    card

--- meal-prep/app/page.js
+++ meal-prep/app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Nav from "@/components/Nav";
 import Banner from "@/components/Banner";
+import Recipe from "@/components/Recipe";
+
+
 export default function Home() {
   return (
-    <>
-    <Nav />
-    <Banner/>
-    </>
+    <main className="bg-gradient-to-b from-slate-100 via-slate-200 to-slate-300">
+      <Nav />
+      <Banner/>
+      <Recipe />
+    </main>
   );
 }
---
+++ meal-prep/components/Recipe.jsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
+
+export default function MultiActionAreaCard(props) {
+  return (
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="/assets/logo.png"
+          alt="food"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.food}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.description}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Close
+        </Button>
+      </CardActions>
+    </Card>
+  );
+}
