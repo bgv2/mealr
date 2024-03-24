54be7ea8a13b8f6cc45dee8e54f7fe03a5aae8c6
aryan1fatemi <sf84561@email.vccs.email.edu>
2024-03-24 09:36:09 -0400

    .env

--- meal-prep/pages/api/findrecipe.js
+++ meal-prep/pages/api/findrecipe.js
@@ -1,23 +1,28 @@
-import { configDotenv } from 'dotenv';
-import { NextResponse } from 'next/server';
-import url from 'url';
-
-const { edamamAppID, edamamAppKey } = process.env;
+import fetch from 'node-fetch';
 
 export default async function handler(req, res) {
-    let queryObj = {
-        ...req.query,
-        type: 'public',
-        app_id: edamamAppID,
-        app_key: edamamAppKey,
-        random: true,
-    }
+  const { health } = req.query;
+  const edamamAppID = process.env.edamamAppID;
+  const edamamAppKey = process.env.edamamAppKey;
 
-    const requestUrl = new URL("https://api.edamam.com/api/recipes/v2")
+  try {
+    let queryObj = {
+      type: 'public',
+      app_id: edamamAppID,
+      app_key: edamamAppKey,
+      random: true,
+      health // Add health parameter from request query
+    };
+
+    const requestUrl = new URL("https://api.edamam.com/api/recipes/v2");
     requestUrl.search = new URLSearchParams(queryObj).toString();
 
     const recipeResp = await fetch(requestUrl.toString());
     const recipeData = await recipeResp.json();
 
     res.status(200).json(recipeData);
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 }
